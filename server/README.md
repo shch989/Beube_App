e75cae0dda7af5d0705a7758a9e866b380a65698
shch989 <tankpop11@gmail.com>
2023-05-02 23:16:47 +0900

    project setting

---
+++ server/README.md
@@ -0,0 +1,34 @@
+# 브베 열샤 앱 개발(Server)
+
+## 대표적인 기능
+
+- 브베 전자도네 모음 영상 재생 기능
+- 열샤 전용 단체 톡방 기능
+- 브베 방송 도방 기능
+
+## 설치 모듈
+
+### NestJS
+
+```
+$ npm i -g @nestjs/cli
+$ nest new server
+```
+
+## Mongoose
+
+```
+$ npm i @nestjs/mongoose mongoose
+```
+
+## Config
+
+```
+$ npm i -g @nestjs/cli
+```
+
+## Class Validator
+
+```
+$ npm i --save class-validator class-transformer
+```
---
+++ server/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+
+// Mongoose Debug
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
